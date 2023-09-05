af93872e97019d7399471d6a0925592d35790fe1
Cheng Haohan <haohan.cheng@shopee.com>
2023-09-05 15:31:07 +0800

    implement basic method in gin, add main methods

---
+++ gee/gee.go
@@ -0,0 +1,42 @@
+package gee
+
+import (
+	"fmt"
+	"net/http"
+)
+
+type HandlerFunc func(http.ResponseWriter, *http.Request)
+
+type Engine struct {
+	router map[string]HandlerFunc
+}
+
+func (engine *Engine) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
+	key := request.Method + "-" + request.URL.Path
+	if handler, ok := engine.router[key]; ok {
+		handler(writer, request)
+	} else {
+		fmt.Fprintf(writer, "404 Not Found: %s\n", request.URL)
+	}
+}
+
+func New() *Engine {
+	return &Engine{router: make(map[string]HandlerFunc)}
+}
+
+func (engine *Engine) addRouter(method string, pattern string, handler HandlerFunc) {
+	key := method + "-" + pattern
+	engine.router[key] = handler
+}
+
+func (engine *Engine) GET(pattern string, handler HandlerFunc) {
+	engine.addRouter("GET", pattern, handler)
+}
+
+func (engine *Engine) POST(pattern string, handler HandlerFunc) {
+	engine.addRouter("POST", pattern, handler)
+}
+
+func (engine *Engine) Run(addr string) (err error) {
+	return http.ListenAndServe(addr, engine)
+}
